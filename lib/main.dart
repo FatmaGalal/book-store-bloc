74bca71470626310de71794bccaa8b9d765cd2b8
Fatma Galal <fatma.galal@linkdev.com>
2026-03-09 22:52:37 +0200

    remove duplicate

--- lib/main.dart
+++ lib/main.dart
@@ -4,9 +4,6 @@ import 'package:book_store/src/core/cubits/locale_cubit/locale_cubit.dart';
 import 'package:book_store/src/core/helpers/init_hive.dart';
 import 'package:book_store/src/core/services/setup_dependencies.dart';
 import 'package:book_store/src/features/authentication/presentation/pages/signup_page.dart';
-import 'package:book_store/src/features/home/data/repos/home_repo_impl.dart';
-import 'package:book_store/src/features/home/domain/use_cases/fetch_book_list_use_case.dart';
-import 'package:book_store/src/features/home/presentation/cubits/books_listing_cubits/books_listing_cubit.dart';
 import 'package:book_store/src/features/home/presentation/cubits/favorites_books_cubits/favorites_books_cubit.dart';
 import 'package:book_store/src/features/home/presentation/pages/book_details_page.dart';
 import 'package:book_store/src/features/home/presentation/pages/book_listing_page.dart';
@@ -34,11 +31,6 @@ class BookStoreApp extends StatelessWidget {
     return MultiBlocProvider(
       providers: [
         BlocProvider(create: (context) => FavoritesBooksCubit()),
-        BlocProvider(
-          create: (context) => BooksListingCubit(
-            FetchBookListUseCase(homeRepo: getIt.get<HomeRepoImpl>()),
-          ),
-        ),
         BlocProvider(create: (context) => LocaleCubit()),
       ],
       child: BlocBuilder<LocaleCubit, LocaleState>(
