dd828b30379a38117bd3716c7dbd024d5dc63e38
Fatma Galal <fatma.galal@linkdev.com>
2026-03-25 02:56:18 +0200

    feat: implement adaptive and responsive UI layout

--- lib/src/core/components/custom_button.dart
+++ lib/src/core/components/custom_button.dart
@@ -1,26 +1,33 @@
-
 import 'package:book_store/src/core/constants/constants.dart';
 import 'package:flutter/material.dart';
 
-class CustomButton extends StatelessWidget{
- const CustomButton({super.key,  this.onTap, required  this.buttonText});
- final String buttonText;
- final VoidCallback? onTap;
- 
+class CustomButton extends StatelessWidget {
+  const CustomButton({super.key, this.onTap, required this.buttonText});
+  final String buttonText;
+  final VoidCallback? onTap;
+
   @override
   Widget build(BuildContext context) {
-   
-   return  GestureDetector(
-    onTap: onTap,
-     child: Container(
-                  width: double.infinity,
-                  height: 60,
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(10),
-                    color: kPrimaryColor,
-                  ),
-                  child: Center(child: Text(buttonText,style: TextStyle(fontSize: 20, color: kButtonFontColor, fontWeight: FontWeight.w800),)),
-                ),
-   );
+    return GestureDetector(
+      onTap: onTap,
+      child: Container(
+        width: double.infinity,
+        height: 60,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(10),
+          color: kPrimaryColor,
+        ),
+        child: Center(
+          child: Text(
+            buttonText,
+            style: TextStyle(
+              fontSize: 20,
+              color: kButtonFontColor,
+              fontWeight: FontWeight.w800,
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
--- lib/src/features/home/presentation/widgets/favorite_book_body.dart
+++ lib/src/features/home/presentation/widgets/favorite_book_body.dart
@@ -15,20 +15,40 @@ class FavoriteBookBody extends StatefulWidget {
 class _FavoriteBookBodyState extends State<FavoriteBookBody> {
   @override
   Widget build(BuildContext context) {
+    final screenWidth = MediaQuery.sizeOf(context).width;
+    final maxTileWidth = screenWidth >= 1200
+        ? 300.0
+        : screenWidth >= 800
+        ? 260.0
+        : 220.0;
+    final childAspectRatio = screenWidth >= 1200
+        ? 0.98
+        : screenWidth >= 800
+        ? 0.92
+        : 0.86;
+
     return BlocBuilder<FavoritesBloc, FavoritesState>(
       builder: (context, state) {
         if (state is FavoritesBooksLoaded && state.favorites.isEmpty) {
           return const Center(child: Text('No favorite books yet'));
         }
+
         return ModalProgressHUD(
           inAsyncCall: state is FavoritesBooksLoaded ? false : true,
           progressIndicator: CircularProgressIndicator(color: kPrimaryColor),
           child: GridView.builder(
+            padding: EdgeInsets.symmetric(
+              horizontal: screenWidth >= 800 ? 24 : 12,
+              vertical: 12,
+            ),
             itemCount: state is FavoritesBooksLoaded
                 ? state.favorites.length
                 : 0,
-            gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-              crossAxisCount: 2,
+            gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+              maxCrossAxisExtent: maxTileWidth,
+              mainAxisSpacing: 16,
+              crossAxisSpacing: 16,
+              childAspectRatio: childAspectRatio,
             ),
             itemBuilder: (context, index) {
               final book = state is FavoritesBooksLoaded
